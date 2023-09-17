228ab4c00886ef567c4ea0fe3a15772b98c01d3d
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-17 14:29:33 +0300

    integrated styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 20px;
+`;
+
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button>Check in</Button>
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
-export default App
+export default App;
