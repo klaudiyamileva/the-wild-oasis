17a538f861cb01257048176f209b58d44be0e987
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-19 12:25:12 +0300

    displayed cabins and deleting cabins

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,20 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
 
-      <img src="https://kbaudkikxgtswsbucysk.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" />
-    </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
