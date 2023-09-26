c021974dd5fdbab28cf4d4c3d5ee272943bb1e8a
klaudiyamileva <klaudiyamileva@gmail.com>
2023-09-26 12:23:29 +0300

    transformed filter component to reusable

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,12 +1,19 @@
-import Filter from "../../ui/Filter"
-import TableOperations from "../../ui/TableOperations"
+import Filter from "../../ui/Filter";
+import TableOperations from "../../ui/TableOperations";
 
 function CabinTableOperations() {
-    return (
-        <TableOperations>
-            <Filter></Filter>
-        </TableOperations>
-    )
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
+        ]}
+      />
+    </TableOperations>
+  );
 }
 
-export default CabinTableOperations
+export default CabinTableOperations;
