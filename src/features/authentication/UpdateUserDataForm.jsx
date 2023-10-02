632d16d57211b03f5e44b2792f62316d77363f61
klaudiyamileva <klaudiyamileva@gmail.com>
2023-10-02 10:03:41 +0300

    builded the app header

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -9,7 +9,6 @@ import Input from "../../ui/Input";
 import { useUser } from "./useUser";
 
 function UpdateUserDataForm() {
-  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
   const {
     user: {
       email,
